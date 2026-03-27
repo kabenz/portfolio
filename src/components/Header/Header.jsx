c081c648751381330e93f8eacd5cef3490d60a7f
jose-perdigao-osv_fedex <jose.perdigao.osv@fedex.com>
2026-03-27 18:00:58 +0000

    fix: 🐛 header order

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,8 @@ const navLinks = [
   { label: 'About', href: '#about' },
   { label: 'Skills', href: '#skills' },
   { label: 'Experience', href: '#experience' },
-  { label: 'Projects', href: '#projects' },
   { label: 'Education', href: '#education' },
+  { label: 'Projects', href: '#projects' },
   { label: 'Hobbies', href: '#hobbies' },
   { label: 'Contact', href: '#contact' },
 ]
